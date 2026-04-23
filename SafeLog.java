06a175a087682c5e2e1c31875d7bf47d3b0c76a6
riyabaranwal <riya.rb203@gmail.com>
2026-04-23 20:08:02 +0530

    changes made in print statement

--- SafeLog.java
+++ SafeLog.java
@@ -44,10 +44,19 @@ public class SafeLog{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String password;
+        System.out.println("--------SafeLog Employee Portal------");
+        System.out.println();
+        System.out.println("Please create a strong password that meets the following criteria:");
+        System.out.println();
+        System.out.println("1. At least 8 characters long.");
+        System.out.println("2. Contains at least one Uppercase letter.");
+        System.out.println("3. Contains at least one Digit (0-9).");
+        
         while(true){
-            System.out.println("Enter the password : ");
+            System.out.println();
+            System.out.print("Enter the password : ");
             password = sc.nextLine();
-            if(!passwordValidator.validatePassword(password)){ System.out.println("Please enter the correct password");}
+            if(!passwordValidator.validatePassword(password)){ System.out.println("\nPassword Invalid. Please fix all the above issues, then enter the password according to requirement");}
             else{ 
                 System.out.println("Password is valid");
                 break;
